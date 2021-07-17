5ca0785dbefe2d0e462ac4e2e4cfb915e9b481a9
Conqueror14M <dy6579@srmist.edu.in>
2021-07-17 19:48:48 +0530

    FIX: typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,7 @@ function Navbar() {
                 <Link to="/" className='nav-links' onClick={closeMobileMenu}>Home</Link>
               </li>
               <li className="nav-item">
-                <Link to="/servives" className='nav-links' onClick={closeMobileMenu}>Services</Link>
+                <Link to="/services" className='nav-links' onClick={closeMobileMenu}>Services</Link>
               </li>
               <li className="nav-item">
                 <Link to="/products" className='nav-links' onClick={closeMobileMenu}>Products</Link>
